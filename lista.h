7155711ae34d37dc4befd1e46c484fb37b51d62d
Fabio Duarte <fbduarte.ic@gmail.com>
2025-06-23 20:00:58 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -1,9 +1,12 @@
-struct Node {
-    char key[15];
-    int value;
-    struct Node* next;
-} ;
+#ifndef LISTA_H
+#define LISTA_H
 
-struct list {
-    struct Node* first;
-};
+typedef struct list List;
+
+List* new_list();
+void free_list(List* l);
+void set_variable(List* l, const char* key, int value);
+int get_variable(List* l, const char* key, int* found);
+void print_variables(List* l);
+
+#endif
