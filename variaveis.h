211143c855e75f1a2a0041c2120b337e0d0ca531
Fabio Duarte <fbduarte.ic@gmail.com>
2025-06-23 20:01:43 -0300

    Update variaveis.h

--- variaveis.h
+++ variaveis.h
@@ -1,13 +1,1 @@
-#ifndef VARIAVEIS_H
-#define VARIAVEIS_H
 
-typedef struct Variavel {
-    char nome[50];          
-    int valor;              
-    struct Variavel* proximo; 
-} Variavel;
-
-void store_variavel(const char* nome, int valor);
-Variavel* load_variavel(const char* nome);
-
-#endif
